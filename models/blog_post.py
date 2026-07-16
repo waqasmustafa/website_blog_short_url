461d8775e87d201331bc2d49067458cacf273267
waqasmustafa <mustafawaqas0@gmail.com>
2026-07-17 00:25:08 +0500

    Override blog canonical URL to stop SEO auto-redirects

--- models/blog_post.py
+++ models/blog_post.py
@@ -13,6 +13,23 @@ def custom_slugify(s):
     return s
 
 
+class BlogBlog(models.Model):
+    _inherit = "blog.blog"
+
+    def _compute_website_url(self):
+        super()._compute_website_url()
+        for blog in self:
+            if blog.id:
+                # If there's only 1 blog for this website, its URL is just /blog
+                domain = [('website_id', 'in', (False, blog.website_id.id))] if blog.website_id else []
+                blogs_count = self.env['blog.blog'].search_count(domain)
+                
+                if blogs_count == 1:
+                    blog.website_url = "/blog"
+                else:
+                    blog.website_url = "/blog/%s" % custom_slugify(blog.name)
+
+
 class BlogPost(models.Model):
     _inherit = "blog.post"
 

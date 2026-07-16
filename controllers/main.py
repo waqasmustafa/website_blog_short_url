3e714c020bc6194b08e60837efb8a2cc5add609b
waqasmustafa <mustafawaqas0@gmail.com>
2026-07-17 00:02:21 +0500

    fixed issue

--- controllers/main.py
+++ controllers/main.py
@@ -1,12 +1,24 @@
 from odoo import http
 from odoo.addons.website_blog.controllers.main import WebsiteBlog
+from odoo.addons.http_routing.models.ir_http import slugify
 from odoo.http import request
 
 
 class WebsiteBlogShortURL(WebsiteBlog):
 
+    @http.route([
+        '/blog',
+        '/blog/page/<int:page>',
+    ], type='http', auth="public", website=True, sitemap=True)
+    def blogs(self, page=1, **post):
+        # Override to prevent redirect to /blog/our-blog-1 if there is only 1 blog
+        blogs = request.env['blog.blog'].search([('website_id', 'in', (False, request.website.id))])
+        if len(blogs) == 1:
+            return self.blog(blog=blogs[0], page=page, **post)
+        return super().blogs(page=page, **post)
+
     @http.route(
-        ['/blog/<model("blog.post"):blog_post>'],
+        ['/blog/<string:post_slug>'],
         type="http",
         auth="public",
         website=True,
@@ -14,12 +26,30 @@ class WebsiteBlogShortURL(WebsiteBlog):
     )
     def short_blog_post(
         self,
-        blog_post,
+        post_slug,
         tag_id=None,
         page=1,
         enable_editor=None,
         **post
     ):
+        # Find the blog post by checking its slugified name instead of its ID
+        all_posts = request.env['blog.post'].search([])
+        blog_post = all_posts.filtered(lambda p: slugify(p.name or '') == post_slug)
+        
+        if not blog_post:
+            # Fallback for old links that might still have the ID in them
+            try:
+                _, post_id = request.env['ir.http']._unslug(post_slug)
+                if post_id:
+                    blog_post = request.env['blog.post'].browse(post_id)
+            except Exception:
+                pass
+
+        if not blog_post or not blog_post.exists():
+            raise request.not_found()
+            
+        blog_post = blog_post[0]
+
         return super().blog_post(
             blog=blog_post.blog_id,
             blog_post=blog_post,
--- models/blog_post.py
+++ models/blog_post.py
@@ -1,4 +1,5 @@
 from odoo import models
+from odoo.addons.http_routing.models.ir_http import slugify
 
 
 class BlogPost(models.Model):
@@ -8,4 +9,4 @@ class BlogPost(models.Model):
         super()._compute_website_url()
         for blog_post in self:
             if blog_post.id:
-                blog_post.website_url = "/blog/%s" % self.env["ir.http"]._slug(blog_post)
+                blog_post.website_url = "/blog/%s" % slugify(blog_post.name or '')
